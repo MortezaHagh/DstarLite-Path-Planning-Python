1d0801aad28aca49ec6241fa5c635932f078502b
MortezaHagh <m.haghbeigi@gmail.com>
2022-06-28 21:56:03 +0430

    debug: top_key

--- top_key.py
+++ top_key.py
@@ -3,11 +3,9 @@ import numpy as np
 
 def top_key(open):
 
-    keys = np.array([op.key for op in open.list])
-    keys = keys[-1::-1][:]
+    keys = np.array([[np.random.rand(1)[-1], op.key[1], op.key[0]]
+                    for op in open.list])
     ind = np.lexsort(keys.T)
-    ind = ind[-1::-1]
-
     top_node = open.list[ind[0]]
     top_node.ind = ind[0]
 
